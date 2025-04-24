bd4fb5f612aa168b15397b91736ec4b99e3733ab
VitorPaes-coder <vpaes3197@gmail.com>
2025-04-24 10:01:10 -0300

    tudo bao

---
+++ js/main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import { getContatos, getContatosPorNome } from "./contato.js"
+
+async function criarCard(contato){
+    const container = document.getElementById('container')
+    const card = document.createElement('div')
+    card.classList.add('card-contato')
+    card.innerHTML = `
+                        <img src="${contato.foto}" alt="foto do contato">
+                        <h2>${contato.nome}</h2>
+                        <p>${contato.celular}</p>
+    `
+    container.appendChild(card)
+}
+
+async function exibirContatos(){
+    const contatos = await getContatos()
+    contatos.forEach(criarCard)
+}
+
+async function exibirPesquisa(evento) {
+    const container = document.getElementById('container')
+    if (evento.key == 'Enter') {
+        const contatos = await getContatosPorNome(evento.target.value)
+        container.replaceChildren('')
+        contatos.forEach(criarCard)
+    }
+ 
+    
+}
+
+document.getElementById('pesquisar').addEventListener('keydown', exibirPesquisa)
+
+exibirContatos()
