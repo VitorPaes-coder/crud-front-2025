f38be36bd4a0dc3597b3cd269e7074597e880634
VitorPaes-coder <vpaes3197@gmail.com>
2025-04-29 15:43:50 -0300

    melhorias

--- js/jogo.js
+++ js/jogo.js
@@ -1,41 +1,35 @@
 'use strict'
 
-export async function getContatos(){
-    const url = `https://bakcend-fecaf-render.onrender.com/contatos`
+export async function getJogos(){
+    const url = `http://localhost:8080/v1/controle-jogos/jogo`
     const response = await fetch (url)
     const data = await response.json()
     return data
 }
 
-export async function getContatosPorNome(nome){
-    const url = `https://bakcend-fecaf-render.onrender.com/contatos/?nome_like=^${nome}`
+export async function getJogoPorId(id){
+    const url = `http://localhost:8080/v1/controle-jogos/${id}`
     const response = await fetch (url)
     const data = await response.json()
     return data
 }
 
-async function getContato(id){
-    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
-    const response = await fetch (url)
-    const data = await response.json()
-    console.log(data);
-    return data
-}
-
-async function postContato(contato){
-    const url = `https://bakcend-fecaf-render.onrender.com/contatos/`
+/** 
+async function postJogo(jogo){
+    const url = `http://localhost:8080/v1/controle-jogos/`
     const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(contato)
+        body: JSON.stringify(jogo)
     }
     const response = await fetch(url, options)
     return response.ok
 }
 
-const novoContato = {
+
+const novoJogo = {
     "nome": "Vitor Paes Rodrigues",
     "celular": "11 40028922",
     "foto": "../img/vitor-paes.png",
@@ -57,6 +51,7 @@ async function putContato(contato, id){
     return response.ok
 }
 
+
 async function deleteContato(id){
     const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
     const options = {
@@ -65,3 +60,4 @@ async function deleteContato(id){
     const response = await fetch(url, options)
     return response.ok
 }
+**/
--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
 'use strict'
 
-import { getContatos, getContatosPorNome } from "./contato.js"
+import { getJogos, getJogoPorId } from "./jogo.js"
 
-async function criarCard(contato){
+async function criarCard(jogo){
     const container = document.getElementById('container')
     const card = document.createElement('div')
-    card.classList.add('card-contato')
+    card.classList.add('card-jogo')
     card.innerHTML = `
-                        <img src="${contato.foto}" alt="foto do contato">
-                        <h2>${contato.nome}</h2>
-                        <p>${contato.celular}</p>
+                        <img src="${jogo.foto}" alt="foto do contato">
+                        <h2>${jogo.nome}</h2>
+                        <p>${jogo.celular}</p>
     `
     container.appendChild(card)
 }
 
-async function exibirContatos(){
-    const contatos = getContatos()
-    contatos.forEach(criarCard)
+async function exibirJogos(){
+    const jogos = getJogos()
+    jogos.forEach(criarCard)
 }
 
 async function exibirPesquisa(evento) {
     const container = document.getElementById('container')
     if (evento.key == 'Enter') {
-        const contatos = await getContatosPorNome(evento.target.value)
+        const jogos = await getContatosPorNome(evento.target.value)
         container.replaceChildren('')
         contatos.forEach(criarCard)
     }
- 
-    
 }
 
 document.getElementById('pesquisar').addEventListener('keydown', exibirPesquisa)
 
-exibirContatos()
+exibirJogos()
